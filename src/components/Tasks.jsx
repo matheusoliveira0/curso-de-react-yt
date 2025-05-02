eb052d2588afa94739ee527c002b9d2c1bbf09df
Matheus Oliveira <teumatheus.73@gmail.com>
2025-05-02 17:20:13 -0300

    inluido botao de check

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { CheckIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
@@ -16,8 +16,9 @@ function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
         <li key={task.id} className="flex gap-2">
           <button
             onClick={() => onTaskClick(task.id)}
-            className={`bg-slate-400 w-full text-left text-white p-2 rounded-md ${task.isCompleted && 'line-through'}`}
+            className={`bg-slate-400 w-full text-left flex items-center gap-2 text-white p-2 rounded-md ${task.isCompleted && 'line-through'}`}
           >
+            {task.isCompleted && <CheckIcon/>}
             {task.title}
           </button>
           <Button
